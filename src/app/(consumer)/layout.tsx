0b6b9b6ff1708e4ccf3d690041304d17c0caf043
Hien Vu <vuvinhhien.work@gmail.com>
2025-08-25 20:21:19 +0700

    Left off work

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -1,4 +1,6 @@
+import { getCurrentUser } from "@/app/services/clerk";
 import { Button } from "@/components/ui/button";
+import { canAccessAdminPages } from "@/permissions/general";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { ReactNode, Suspense } from "react";
@@ -26,6 +28,8 @@ function NavBar() {
         </Link>
         <Suspense>
           <SignedIn>
+            <AdminLink />
+
             <Link
               className="hover:bg-accent/10 flex items-center px-2"
               href="/courses"
@@ -62,5 +66,19 @@ function NavBar() {
         </Suspense>
       </nav>
     </header>
+  );
+}
+
+async function AdminLink() {
+  const user = await getCurrentUser();
+
+  if (!canAccessAdminPages(user)) {
+    return null;
+  }
+
+  return (
+    <Link className="hover:bg-accent/10 flex items-center px-2" href="/admin">
+      Admin
+    </Link>
   );
 }
---
+++ src/features/users/db/cache.ts
@@ -0,0 +1,7 @@
+export function getUserGlobalTag() {
+  return "users";
+}
+
+export function getUserIdTag(id: string) {
+  return "users" + id;
+}
